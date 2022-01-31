def91211b97b287532737b7a8d047affb2535bec
seb997a <seb997a@hotmail.com>
2022-01-31 17:58:27 -0500

    Added container for the main conent
    Added the container as well as making some css classes for formatting. Creating some sample text  that was held inside the container.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,21 +12,20 @@ function Header() {
     <>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Brainy Shadow</Navbar.Brand>
+          <Navbar.Brand href="home">Brainy Shadow</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="projects">About Me</Nav.Link>
+              <Nav.Link href="about">About Me</Nav.Link>
               <NavDropdown title="Projects" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">DDB V8</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
+                <NavDropdown.Item href="project1">DDB V8</NavDropdown.Item>
+                <NavDropdown.Item href="project2">
                   Project 2
                 </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
+                <NavDropdown.Item href="project3">
                   Project 3
                 </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
+                <NavDropdown.Item href="project4">
                   Project 4
                 </NavDropdown.Item>
               </NavDropdown>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Nav from "../components/Nav";
-
+import "../App.css"
 
 function Home() {
   return (
     <>
-      <div><Nav/></div>
+      <div>
+        <Nav />
+      </div>
+      <div className="content-container" id="main-area">
+        <p className="body-text">Hi, welcome to my website!</p>
+      </div>
     </>
   );
 }
