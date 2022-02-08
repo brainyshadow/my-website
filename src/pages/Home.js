cc94b7728aba564575545f2d1e503f08c4d21ad2
seb997a <seb997a@hotmail.com>
2022-02-07 22:23:45 -0500

    Added a background colour to the homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,34 +7,40 @@ import ProjectCard from "../components/ProjectCard";
 function Home() {
   return (
     <>
-      <div>
+      <div className="grey-container">
         <Nav />
-      </div>
-      <div className="content-container" id="main-area">
-        <p className="body-text">Hi, welcome to my website!</p>
-      </div>
-      <div className="project-container" id="main-area">
-        <ProjectCard
-          complexity="Medium"
-          name="DDB V8"
-          videoLink="https://youtu.be/SYQoG84IRUQ"
-          projectLink="https://github.com/seb997a/DDB"
-          description="A developemnt platform that allows easy programming over USB using a open-source bootloader."
-        />
-        <ProjectCard
-          complexity="Medium"
-          name="LED Controller"
-          videoLink="https://www.youtube.com/watch?v=PgKoRlew9HA&ab_channel=SebA"
-          projectLink=""
-          description="A LED controller that controlls an addresable LED strip that can change colors based on surrounding music."
-        />
-        <ProjectCard
-          complexity="Medium"
-          name="Fooseball Scoreboard"
-          videoLink="https://www.youtube.com/watch?v=ESi4YN3UO08&ab_channel=SebA"
-          projectLink=""
-          description="A fooseball scoreboard that automatically senses goals and keeps track of the game's score."
-        />
+
+        <div className="title-container" id="main-area">
+          <p className="body-text">Hi, welcome to my website!</p>
+          <p className="body-text">
+            Here, I post some of my cool projects as well as practice my web
+            developemnt and react skills.
+          </p>
+        </div>
+        <div className="content-container"></div>
+        <div className="project-container" id="main-area">
+          <ProjectCard
+            complexity="Medium"
+            name="DDB V8"
+            videoLink="https://youtu.be/SYQoG84IRUQ"
+            projectLink="https://github.com/seb997a/DDB"
+            description="A developemnt platform that allows easy programming over USB using a open-source bootloader."
+          />
+          <ProjectCard
+            complexity="Medium"
+            name="LED Controller"
+            videoLink="https://www.youtube.com/watch?v=PgKoRlew9HA&ab_channel=SebA"
+            projectLink=""
+            description="A LED controller that controlls an addresable LED strip that can change colors based on surrounding music."
+          />
+          <ProjectCard
+            complexity="Medium"
+            name="Fooseball Scoreboard"
+            videoLink="https://www.youtube.com/watch?v=ESi4YN3UO08&ab_channel=SebA"
+            projectLink=""
+            description="A fooseball scoreboard that automatically senses goals and keeps track of the game's score."
+          />
+        </div>
       </div>
     </>
   );
